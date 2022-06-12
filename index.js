6ffa8ced304fc1620da155dba83bc5e102757dd3
Amos Koech <koechronix@yahoo.com>
2022-06-11 20:17:22 -0400

    commit tree 7b9313bf8bf7efd7cde629eabdc34bf7190ae2ee

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(driver, name) {
+    return driver.filter(function(driver){
+        return driver.toLowerCase()===name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function(element) {
+        return element.slice(0, 1) ===string.slice(0,1)
+    });
+}
+function matchName(array, name){
+    return array.filter(function(driver){
+        return driver.name === name;
+    });
+}
